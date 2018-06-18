da1d80c8645ee89fd30ae3888fba7f141cd37dd3
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-18 08:49:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 
 def assign_rooms(speakers)
   speakers_with_rooms = []
-  index = 0
+  index = 1
   speakers.each_with_index do |name, index|
     speakers_with_rooms.push("Hello, #{name}! You'll be assigned to room #{index}!")
   end
